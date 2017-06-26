9464b652aa69859074195f2e613ab9b7f589c1d0
Artem <kharitonov1995@gmail.com>
2017-06-26 14:44:59 +0600

    реализация free()

--- malloc/lib/malloc.c
+++ malloc/lib/malloc.c
@@ -1,5 +1,7 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "../include/malloc.h"
 #define CHUNK_SIZE sizeof(Chunk)
@@ -12,6 +14,7 @@ void init_hook(void) {
 }
 
 void *m_malloc_hook(size_t size, const void *caller) {
+	void *ptr;
 	Chunk *chunk = NULL, *last = NULL;
 	size = align(size);
 	if (head == NULL) {
@@ -27,29 +30,43 @@ void *m_malloc_hook(size_t size, const void *caller) {
 				return NULL;
 		}
 	}
-        printf("allocate memory: \n0x%x %d\n", chunk, chunk->size);
+	ptr = sbrk(0);
+        printf("allocate memory: \n%p size = %d\nnext addr: %p\n",
+			(void *) chunk, (int)chunk->size, (void *) ptr);
+
         return (void *)(chunk + CHUNK_SIZE);
 }
 
 void m_free_hook(void *ptr, const void *caller) {
-        printf("321\n");
-        free(ptr);
+	Chunk *list = head;
+	Chunk *free_chunk = (Chunk *) ptr - CHUNK_SIZE;
+
+	while(list != NULL) {
+		if (list == free_chunk) {
+			list->free = 0;
+			printf("pointer with addr %p is free\n", (void *) list);
+			return;
+		}
+		list = list->next;
+	}
 }
 
 int align(int size) {
 	if (size % 8 == 0)
 		return size;
-	else {
+	else
 		return (size + 8 - size % 8);
-	}
 }
 
 Chunk *find_chunk(Chunk **last, size_t size) {
 	Chunk *chunk = head;
-	while (chunk != NULL && (chunk->size >= size && chunk->free == 0)) {
+	while (chunk->size < size || chunk->free == 1) {
 		*last = chunk;
 		chunk = chunk->next;
+		if (chunk == NULL)
+			return NULL;
 	}
+	chunk->free = 1;
 	return chunk;
 }
 
@@ -63,6 +80,6 @@ Chunk *extend_heap(Chunk *last, size_t size) {
 	chnk->next = NULL;
 	if (last)
 		last->next = chnk;
-	chnk->free = 0;
+	chnk->free = 1;
 	return chnk;
 }
--- malloc/src/main.c
+++ malloc/src/main.c
@@ -1,11 +1,21 @@
 #include "../lib/malloc.c"
 
 int main() {
-	int *arr, n = 10, i;
+	int *arr, *arr1, n = 10, i;
 	arr = malloc(n);
+	arr1 = malloc(n);
 	for (i = 0; i < n; i++) {
 		arr[i] = i + 1;
+		arr1[(n - 1) - i] = i;
 	}
+	for (i = 0; i < n; i++)
+		printf("arr[%d] = %d\n", i, arr[i]);
+	for (i = 0; i < n; i++)
+		printf("arr1[%d] = %d\n", i, arr1[i]);
+
+	free(arr);
+
+	arr = malloc(n);
 	for (i = 0; i < n; i++)
 		printf("arr[%d] = %d\n", i, arr[i]);
 	return 0;
